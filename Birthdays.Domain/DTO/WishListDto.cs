07ebb4b02961a16040ccb1dccd2b7eb14efcfbbd
Степан <sb.9226981433@gmail.com>
2024-02-27 10:55:30 +0500

    Added Id field to WishList Dto

--- Birthdays.Domain/DTO/WishListDto.cs
+++ Birthdays.Domain/DTO/WishListDto.cs
@@ -2,6 +2,7 @@
 
 public record WishListDto
 {
+    public required Guid Id { get; init; }
     public required string Name { get; init; }
     public string? Description { get; init; }
     public required WishDto[] Wishes { get; init; } = Array.Empty<WishDto>();
